efaa559267a8a14d8a58875d516e45f1eb516060
Dsfr62 <saviodudu79@gmail.com>
2023-05-12 09:33:57 -0300

    funcionando 100% sem css

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-
-
 let form = document.querySelector("form")
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
     const submitedButton = event.submitter.id
+    const first_number = Number(document.getElementById("first-number").value)
+    const second_number = Number(document.getElementById("second-number").value)
+
+    let resultado;
+
+    if(submitedButton == "adicao") {
+        resultado = `Resultado da adição: ${first_number + second_number}`
+    } else if(submitedButton == "subtracao") {
+        resultado = `Resultado da subtração ${first_number - second_number}`
+    } else if(submitedButton == "multiplicacao") {
+        resultado = `Resultado da multiplicação: ${first_number * second_number}`
+    } else if(submitedButton == "divisao") {
+        if(second_number == 0) {
+            alert("Não é possível efetuar a divisão por zero")
+            resultado = ""
+        } else {
+            resultado = `Resultado da divisão: ${first_number / second_number}`
+        }
+    }
+
+    document.getElementById("resultado").innerHTML = resultado
 })
